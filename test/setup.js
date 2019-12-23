080df5039ce99f96f27d6e15eff2b7ef76739132
India Rodriguez <carnes.s.ia17@gmail.com>
2019-12-22 21:28:39 -0700

    tests are passing for jwt. Not getting user_id in login request.

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
 process.env.TZ = 'UTC'
 process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
     || "postgresql://thingful@localhost/thingful-test"
